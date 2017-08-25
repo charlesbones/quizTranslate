db3bdd0fb39f90b91f46de0af949ac559e7dee5e
Carlos Rodriguez <c.rodriguez@arduino.cc>
2017-08-25 13:17:41 +0200

    new line skip

--- toMoodle.py
+++ toMoodle.py
@@ -5,20 +5,23 @@ try:
     path=sys.argv[1]
     files = glob.glob(path+"*")
     os.makedirs(path+"moodle/")
+    count=0
     for fle in files:
         origin=open(fle,'r');
         result=open(path+"moodle/"+os.path.basename(fle[:-4])+"_moodle.txt",'w+');
         for line in origin:
             if line.find("QQQ") != -1:
+                line=line.replace("\n","")
                 result.write("// question:"+line[3:len(line)])
             elif line.find("SSS") != -1:
                 result.write("name:"+line[3:len(line)])
             elif line.find("RRR ") != -1:
+                line=line.replace("\n"," ")
                 result.write("::"+line[4:len(line)])
             elif line.find("RRR ") != -1:
                 result.write("::"+line[4:len(line)])
             elif line.find("*---") != -1:
-                result.write("{"+'\n')
+                result.write("\n")
             elif line.find("CCC ") != -1:
                 result.write('\t'+"~[moodle]"+line[4:len(line)])
             elif line.find("CCC50 ") != -1:
